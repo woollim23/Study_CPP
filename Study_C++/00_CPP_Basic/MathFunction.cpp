e2a8a1fa633a2275d58d396de468f7edd66dd3ff
woollim <137277221+woollim23@users.noreply.github.com>
2025-12-01 23:38:45 +0900

    update : MathFunction - % 기능 추가

--- Study_C++/00_CPP_Basic/MathFunction.cpp
+++ Study_C++/00_CPP_Basic/MathFunction.cpp
@@ -30,13 +30,25 @@ int main()
 	/* 절대값과 제곱수 구하기(abs, fabs, pow) */
 	cout << endl << "== 절대값 구하기 ==" << endl; // abs : absolute value(절대값)의 약자
 	cout << "-10의 절대값 " << abs(-10) << endl; // 정수의 절대값은 abs 로 구함
-	cout << "-5.72의 절대값" << fabs(-5.72) << endl; // 실수의 절대값은 fabs로 구함, long 자료형은 Labs로 구함
+	cout << "-5.72의 절대값 " << fabs(-5.72) << endl; // 실수의 절대값은 fabs로 구함, long 자료형은 labs로 구함
 
 	cout << endl << "== 제곱수 구하기 ==" << endl;
 	cout << "2의 2승 : " << pow(2, 2) << endl; 
 	cout << "3의 4승 : " << pow(3, 4) << endl;
 	cout << "4의 6승 : " << pow(4, 6) << endl;
 	cout << "8의 3승 : " << pow(8, 3) << endl;
+
+	/* 몫과 나머지 구하기(%) */
+	cout << endl << "== 몫과 나머지 구하기 ==" << endl;
+	double x = 5.7;
+	int div1 = static_cast<int>(x / 5);
+	double mod1 = x - 5 * static_cast<int>(x / 5);
+
+	int y = 10;
+	int div2 = static_cast<int>(y / 2);
+	double mod2 = y % 2;
+
+	cout << "5.7 / 5 = 몫 : " << div1 << "";
 
 	return 0;
 }
