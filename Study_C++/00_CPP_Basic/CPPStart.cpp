f3a868d7d07d0dc43ce196d200eab22180b9cb32
woollim <137277221+woollim23@users.noreply.github.com>
2025-12-23 21:40:55 +0900

    update : CPPStart - 함수 추가

--- Study_C++/00_CPP_Basic/CPPStart.cpp
+++ Study_C++/00_CPP_Basic/CPPStart.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void Minus(const int x, const int y)
+{
+	cout << "x - y = " << x - y << endl;
+}
+
 int main()
 {
-
+	/* 함수 배우기 ( () ) */
+	Minus(10, 5);
 }
