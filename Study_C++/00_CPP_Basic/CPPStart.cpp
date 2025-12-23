d19741cf09d5976b5417227ae274c9a779d8e564
woollim <137277221+woollim23@users.noreply.github.com>
2025-12-23 21:45:49 +0900

    update : CPPStart - 네임스페이스 학습 추가

--- Study_C++/00_CPP_Basic/CPPStart.cpp
+++ Study_C++/00_CPP_Basic/CPPStart.cpp
@@ -6,8 +6,29 @@ void Minus(const int x, const int y)
 	cout << "x - y = " << x - y << endl;
 }
 
+namespace silla
+{
+	int year = 935;
+
+	void CentralArea()
+	{
+		cout << "경상도" << endl;
+	}
+}
+
+using namespace silla;
+
 int main()
 {
 	/* 함수 배우기 ( () ) */
 	Minus(10, 5);
+	cout << endl;
+
+	/* 네임스페이스 배우기 */
+	cout << "신라 중심지 : ";
+	CentralArea();
+	cout << "신라 멸망 연도 : ";
+	cout << year << endl;
+
+	return 0;
 }
