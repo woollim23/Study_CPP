f8030e713e9f42445350c98ad5b877e32660b70f
woollim <137277221+woollim23@users.noreply.github.com>
2025-12-24 23:56:27 +0900

    update : CPPBeginner 포인터 이해 학습 추가

--- Study_C++/00_CPP_Basic/CPPBeginner.cpp
+++ Study_C++/00_CPP_Basic/CPPBeginner.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main()
 {
+	/* 포인터 이해하기(*) */
+	int number1 = 10;
+	int* pointer1 = &number1;
 
+	double number2 = 23.4;
+	double* pointer2 = &number2;
+
+	cout << "number1 : " << number1 << " (size : " << sizeof(number1) << ")" << endl;
+	cout << "pointer1 : " << pointer1 << " (size : " << sizeof(pointer1) << ")" << endl;
+
+	cout << "number2 : " << number2 << " (size : " << sizeof(number2) << ")" << endl;
+	cout << "pointer2 : " << pointer2 << " (size : " << sizeof(pointer2) << ")" << endl;
 	return 0;
 }
