df95270128922625f64a5c0e1eab4b23bcf08901
woollim <137277221+woollim23@users.noreply.github.com>
2026-01-01 21:05:35 +0900

    update : CPPStart - #include 학습 추가

--- Study_C++/00_CPP_Basic/CPPStart.cpp
+++ Study_C++/00_CPP_Basic/CPPStart.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void Minus(const int x, const int y)
@@ -40,7 +41,21 @@ int main()
 	cout << "백제 중심지 : ";
 	baekjae::CentralArea();
 	cout << "백제 멸망 연도 : ";
-	cout << baekjae::year << endl; // 변수 이름이 같아도, 스코프 설정 연산자 앞에 있는 네임스페이스에 따라 달라진다.
+	cout << baekjae::year << endl << endl; // 변수 이름이 같아도, 스코프 설정 연산자 앞에 있는 네임스페이스에 따라 달라진다.
 
+	/* #include 배우기 */
+	// 이미 검증된 외부 라이브러리를 찾아 적용하여 시간 단축 & 효율
+	// vector 라이브러리 활용
+	// C++ 표준 라이브러리에 포함되어 있다면 <>
+	// 새로 작성한 소스코드라면 "경로/파일이름"
+	vector<int> exam;
+	exam.push_back(10);
+	exam.push_back(20);
+	exam.push_back(30);
+
+	for (int i = 0, size = exam.size(); i < size; i++)
+	{
+		cout << "벡터 값 : " << exam.at(i) << endl;
+	}
 	return 0;
 }
