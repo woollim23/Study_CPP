d67a386af90a88102fbe74086978ea9d22bb6ae3
woollim <137277221+woollim23@users.noreply.github.com>
2026-01-02 23:41:43 +0900

    update : CPPBeginner  캐스트 연산자 이해하기

--- Study_C++/00_CPP_Basic/CPPBeginner.cpp
+++ Study_C++/00_CPP_Basic/CPPBeginner.cpp
@@ -15,5 +15,20 @@ int main()
 
 	cout << "number2 : " << number2 << " (size : " << sizeof(number2) << ")" << endl;
 	cout << "pointer2 : " << pointer2 << " (size : " << sizeof(pointer2) << ")" << endl;
+
+
+	/* 캐스트 연산자 이해하기 */
+	int x = 2;
+	double y = 4.4;
+
+	int i = static_cast<int>(y / x);
+	int j = (int)y / x;
+	double k = y / x;
+
+	cout << endl;
+	cout << "4.4 / 2 = (static_cast<int>) " << i << endl;
+	cout << "4.4 / 2 = (int) " << j << endl;
+	cout << "4.4 / 2 = " << k << endl;
+
 	return 0;
 }
