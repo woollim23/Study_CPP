186ae3fdb20ec5701b0ca02ec9ce4aff31e61b48
woollim <137277221+woollim23@users.noreply.github.com>
2026-01-04 23:48:05 +0900

    update : CPPBeginner - 자료형의 크기 이해하기(sizeof)

--- Study_C++/00_CPP_Basic/CPPBeginner.cpp
+++ Study_C++/00_CPP_Basic/CPPBeginner.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+class Temp { // 자료형의 크기 이해하기(sizeof) - 클래스
+	int no;
+	bool is_on;
+};
+
 int main()
 {
 	/* 포인터 이해하기(*) */
@@ -44,6 +49,13 @@ int main()
 	cout << "number2 : " << number2 << endl;
 	cout << "number3 : " << number3 << endl;
 	cout << "number4 : " << number4 << endl;
+
+	/* 자료형의 크기 이해하기(sizeof) */
+	cout << endl;
+	cout << "char 크기 : " << sizeof('p') << endl;
+	cout << "int 크기 : " << sizeof(10) << endl;
+	cout << "double 크기 : " << sizeof(10.0) << endl;
+	cout << "클래스 크기 : " << sizeof(Temp) << endl;
 
 	return 0;
 }
