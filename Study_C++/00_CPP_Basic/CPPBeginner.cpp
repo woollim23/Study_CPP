498ec6bce7f0a553e1f03a9d2a0df5174aa8791b
woollim <137277221+woollim23@users.noreply.github.com>
2026-01-14 23:37:01 +0900

    update : CPPBeginner - 포인터 변수 사용하기

--- Study_C++/00_CPP_Basic/CPPBeginner.cpp
+++ Study_C++/00_CPP_Basic/CPPBeginner.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <algorithm>
+
 using namespace std;
 
-class Temp { // 자료형의 크기 이해하기(sizeof) - 클래스
+class Temp// 자료형의 크기 이해하기(sizeof) - 클래스
+{
 	int no;
 	bool is_on;
 };
 
+int PointerFunc(vector<string>* info) // 포인터 변수 사용하기
+{
+	if (info->empty() == true)
+		return 0;
+	else
+		return sizeof(info);
+}
+
 int main()
 {
-	/* 포인터 이해하기(*) */
-	cout << "== 포인터 이해하기(*) ==" << endl;
-	int number1 = 10;
-	int* pointer1 = &number1;
-
-	double number2 = 23.4;
-	double* pointer2 = &number2;
-
-	cout << "number1 : " << number1 << " (size : " << sizeof(number1) << ")" << endl;
-	cout << "pointer1 : " << pointer1 << " (size : " << sizeof(pointer1) << ")" << endl;
-
-	cout << "number2 : " << number2 << " (size : " << sizeof(number2) << ")" << endl;
-	cout << "pointer2 : " << pointer2 << " (size : " << sizeof(pointer2) << ")" << endl;
-
-
 	/* 캐스트 연산자 이해하기 */
 	int x = 2;
 	double y = 4.4;
@@ -38,8 +35,8 @@ int main()
 	cout << "4.4 / 2 = " << k << endl;
 
 	/* 명시적 변환 이해하기 */
-	number1 = 65;
-	number2 = 23.4;
+	int number1 = 65;
+	double number2 = 23.4;
 
 	int number3 = int(number2);
 	double number4 = double(number1 / number2);
@@ -74,7 +71,6 @@ int main()
 
 
 	/* 포인터 */
-
 	int a = 10;
 	int* p = &a;
 	cout << endl << "== 포인터 ==" << endl;
@@ -82,6 +78,33 @@ int main()
 	cout << "변수 a의 주소: " << &a << endl;
 	cout << "포인터 p가 가리키는 값: " << *p << endl;
 	cout << "포인터 p 자체의 주소: " << &p << endl;
+
+
+	/* 포인터 이해하기(*) */
+	cout << endl << "== 포인터 이해하기(*) ==" << endl;
+	number1 = 10;
+	int* pointer1 = &number1;
+
+	number2 = 23.4;
+	double* pointer2 = &number2;
+
+	cout << "number1 : " << number1 << " (size : " << sizeof(number1) << ")" << endl;
+	cout << "pointer1 : " << pointer1 << " (size : " << sizeof(pointer1) << ")" << endl;
+
+	cout << "number2 : " << number2 << " (size : " << sizeof(number2) << ")" << endl;
+	cout << "pointer2 : " << pointer2 << " (size : " << sizeof(pointer2) << ")" << endl;
+
+
+	/* 포인터 변수 사용하기 */
+	vector<string> message;
+	message.push_back("고려 장군");
+	message.push_back("척준경!");
+	message.push_back("절친 이름은");
+	message.push_back("... 무엇일까");
+
+	cout << endl << "== 포인터 변수 사용하기 ==" << endl;
+	cout << "포인터 인자 크기 : " << PointerFunc(&message) << endl;
+	cout << "원래 변수 크기 : " << sizeof(message) << endl;
 
 	return 0;
 }
