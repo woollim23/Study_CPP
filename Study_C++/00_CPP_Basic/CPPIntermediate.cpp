a9dd9200f39802c79ad0d54a88aeb332daf6ae9f
woollim <137277221+woollim23@users.noreply.github.com>
2026-01-15 22:42:20 +0900

    update : CPPIntermediate - Call by Reference 이해하기

--- Study_C++/00_CPP_Basic/CPPIntermediate.cpp
+++ Study_C++/00_CPP_Basic/CPPIntermediate.cpp
@@ -12,6 +12,18 @@ void Func(int arg)
 	cout << "변경 후 : " << arg << endl;
 }
 
+void Func1(int &arg)
+{
+	cout << "변경 전 : " << arg << endl;
+	arg += 10;
+	cout << "변경 후 : " << arg << endl;
+}
+
+void Func2(string& info)
+{
+	info += "981년";
+}
+
 int main()
 {
 	/* Call by Value 이해하기 */
@@ -19,6 +31,17 @@ int main()
 	Func(year);
 	cout << endl << "== Call by Value 이해하기 ==" << endl;
 	cout << "함수 종료 후 : " << year << endl;
+
+
+	/* Call by Reference 이해하기 */
+	year = 10;
+	Func1(year);
+	cout << endl << "== Call by Reference 이해하기 ==" << endl;
+	cout << "Func1 함수 종료 후 : " << year << endl;
+
+	string king_info = "고려 성종 즉위년 : ";
+	Func2(king_info);
+	cout << king_info << endl;
 
 	return 0;
 }
