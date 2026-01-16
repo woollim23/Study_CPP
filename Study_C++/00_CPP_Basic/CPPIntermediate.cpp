626fd42bd4135da34c240a52582f3905a2424b70
woollim <137277221+woollim23@users.noreply.github.com>
2026-01-16 12:05:07 +0900

    update : CPPIntermediate - const 포인터 이해하기

--- Study_C++/00_CPP_Basic/CPPIntermediate.cpp
+++ Study_C++/00_CPP_Basic/CPPIntermediate.cpp
@@ -71,5 +71,19 @@ int main()
 	cout << question << kMyJob << endl;
 	cout << answer << kMyJob << endl;
 
+
+	/* const 포인터 이해하기 */
+	int number1 = 10;
+	int number2 = 20;
+
+	int const* ptr1;
+	ptr1 = &number1;
+	//ptr1 = number1; 값 변경 불가
+	ptr1 = &number2; // 대상 변경은 가능
+
+	int* const ptr2 = &number1;
+	*ptr2 = number2; // 값 변경 가능
+	//*ptr2 = &number2; // 대상 변경은 불가
+
 	return 0;
 }
