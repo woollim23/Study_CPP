8436177373c318121f21a3b510838a1f0297f1af
woollim <137277221+woollim23@users.noreply.github.com>
2026-01-25 23:25:47 +0900

    가변 배열 복습하기

--- Study_C++/02_Variable_Length_Arrays/Arr_main.cpp
+++ Study_C++/02_Variable_Length_Arrays/Arr_main.cpp
@@ -36,18 +36,18 @@ void BubbleSort(int* _pData, int _iCount)
 
 int main()
 {
-	tArr s = {};
-	InitArr(&s);
+	tArr s = {}; // 구조체 -> 가변배열 정보를 담을 것
+	InitArr(&s); // 가변 배열 초기화
 
-	srand(time(nullptr));
+	srand(time(nullptr)); 
 
 	for (int i = 0; i < 10; i++)
 	{
 		int iRand = rand() % 100 + 1; // 1 ~ 100 값을 랜덤으로
-		PushBack(&s, iRand);
+		PushBack(&s, iRand); // 랜덤값을 배열에 넣음
 	}
 
-	Sort(&s, BubbleSort); // 함수는 이름자체가 주소, & 생략가능
+	Sort(&s, BubbleSort); // 함수는 이름자체가 주소, & 생략가능 -> 함수 포인터
 
 	for (int i = 0; i < s.iCount; i++)
 	{
