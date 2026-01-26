14b9935750890027a1be8ccfd13902a50823c937
woollim <137277221+woollim23@users.noreply.github.com>
2026-01-26 23:32:17 +0900

    연결리스트 복습하기

--- Study_C++/03_Linked_List/LinkedList_main.cpp
+++ Study_C++/03_Linked_List/LinkedList_main.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 int main()
 {
-	tLinkedList list = {};
-	InitList(&list);
+	tLinkedList list = {}; // 리스트 선언
+	InitList(&list); // 리스트 초기화
 
-	PushBack(&list, 100);
+	PushBack(&list, 100); // 리스트에 값 추가
 	PushBack(&list, 200);
 	PushFront(&list, 300);
 
-	tNode* pNode = list.pHeadNode;
+	tNode* pNode = list.pHeadNode; // 헤드 노드 저장
 	for (int i = 0; i < list.iCount; i++)
 	{
-		cout << pNode->iData << " ";
-		pNode = pNode->pNextNode;
+		cout << pNode->iData << " "; 
+		pNode = pNode->pNextNode; // 차례 차례 값 출력후 다음 노드 저장
 	}
 
-	ReleaseList(&list);
+	ReleaseList(&list); // 리스트 메모리 해제
 
 	return 0;
 }
