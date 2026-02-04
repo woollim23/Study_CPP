f745ac1435e5be9f3b58d834887c449551a4f739
woollim <137277221+woollim23@users.noreply.github.com>
2026-02-04 23:41:26 +0900

    update : CPPStruct - 구조체를 함수 인자로 사용하기

--- Study_C++/00_CPP_Basic/CPPStruct.cpp
+++ Study_C++/00_CPP_Basic/CPPStruct.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 using namespace std;
 
+// ====== 구조체를 함수 인자로 사용하기 ======
+struct Princess
+{
+	string name;
+	string father;
+	string birthday;
+}jungso;
+
+void Print(Princess* who)
+{
+	cout << "jungso.name = " << who->name << endl;
+	cout << "jungso.father = " << who->father << endl;
+	cout << "jungso.birthday = " << who->birthday << endl;
+}
+
 int main()
 {
+	// ====== 구조체를 함수 인자로 사용하기 ======
+	cout << endl << "구조체를 함수 인자로 사용하기" << endl;
+	jungso.name = "정소공주";
+	jungso.father = "조선 태종";
+	jungso.birthday = "1412년";
+
+	Print(&jungso);
 
 	return 0;
 }
