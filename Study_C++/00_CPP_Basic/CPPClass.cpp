ef7d4a55c202c49fdda49eca1ab3bc9fdf0ddf11
woollim <137277221+woollim23@users.noreply.github.com>
2026-02-05 00:25:02 +0900

    update : CPPClass - 클래스 정의하기

--- Study_C++/00_CPP_Basic/CPPClass.cpp
+++ Study_C++/00_CPP_Basic/CPPClass.cpp
@@ -2,8 +2,35 @@
 #include <string>
 using namespace std;
 
+// ====== 클래스 정의하기 ======
+
+class KingInfo
+{
+public:
+	void SetName(string name)
+	{
+		name_ = name;
+	}
+
+	string GetName() const
+	{
+		return name_;
+	}
+
+private:
+	string name_;
+};
+
+// =======================================================================================================
+
 int main()
 {
+	// ====== 클래스 정의하기 ======
+	cout << endl << "======클래스 정의하기======" << endl;
+	KingInfo king_info;
+	king_info.SetName("조선 세조 이유");
+
+	cout << king_info.GetName() << endl;
 
 	return 0;
 }
