ccf05b1d40fc4459d2a03f3651a4d0fce7fb8e97
woollim <137277221+woollim23@users.noreply.github.com>
2026-02-05 00:16:14 +0900

    update : CPPStruct - 구조체 초기화하는 방법3

--- Study_C++/00_CPP_Basic/CPPStruct.cpp
+++ Study_C++/00_CPP_Basic/CPPStruct.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 // ====== 구조체를 함수 인자로 사용하기 ======
@@ -46,6 +47,18 @@ struct Data4
 	int number;
 }data4;
 
+// ====== 구조체 초기화하는 방법3 ======
+struct Info
+{
+	string country;
+
+	struct Who
+	{
+		string name;
+		string nickname;
+	}who;
+};
+
 // =======================================================================================================
 
 int main()
@@ -71,6 +84,14 @@ int main()
 
 	cout << "Data3 number : " << data3.number << endl;
 	cout << "Data4 number : " << data4.number << endl;
+
+	// ====== 구조체 초기화하는 방법3 ======
+	cout << endl << "======구조체 초기화하는 방법3======" << endl;
+	Info chuck = { "고려", {"장군 척준경", "소드마스터"} };
+	Info anjang = { "고구려", {"안장왕 고흥안", "한주 사랑"} };
+
+	cout << chuck.country << ", " << chuck.who.name << ", " << chuck.who.nickname << endl;
+	cout << anjang.country << ", " << anjang.who.name << ", " << anjang.who.nickname << endl;
 
 	return 0;
 }
