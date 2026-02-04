c362fe1f787bf33c72e2a49b9a7ddcdae890d7ec
woollim <137277221+woollim23@users.noreply.github.com>
2026-02-05 00:01:36 +0900

    update : CPPStruct - 구조체 초기화하는 방법1

--- Study_C++/00_CPP_Basic/CPPStruct.cpp
+++ Study_C++/00_CPP_Basic/CPPStruct.cpp
@@ -16,15 +16,41 @@ void Print(Princess* who)
 	cout << "jungso.birthday = " << who->birthday << endl;
 }
 
+// ====== 구조체 초기화하는 방법1 ======
+struct Data1
+{
+	Data1(int x) : number(x) {}
+
+	int number;
+};
+
+struct Data2
+{
+	Data2()
+	{
+		number = 10;
+	}
+
+	int number;
+}data2;
+
+// =======================================================================================================
+
 int main()
 {
 	// ====== 구조체를 함수 인자로 사용하기 ======
-	cout << endl << "구조체를 함수 인자로 사용하기" << endl;
+	cout << endl << "======구조체를 함수 인자로 사용하기======" << endl;
 	jungso.name = "정소공주";
 	jungso.father = "조선 태종";
 	jungso.birthday = "1412년";
 
 	Print(&jungso);
+
+	// ====== 구조체 초기화하는 방법1 ======
+	cout << endl << "======구조체 초기화하는 방법1======" << endl;
+	Data1 data1(2);
+	cout << "Data1 number : " << data1.number << endl;
+	cout << "Data2 number : " << data2.number << endl;
 
 	return 0;
 }
