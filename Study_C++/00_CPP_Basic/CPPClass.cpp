a42bdb956e6dd61007e3ad3061211aeb5ca409dc
woollim <137277221+woollim23@users.noreply.github.com>
2026-02-09 11:00:09 +0900

    update : CPPClass - 클래스 생성자 초기화 리스트

--- Study_C++/00_CPP_Basic/CPPClass.cpp
+++ Study_C++/00_CPP_Basic/CPPClass.cpp
@@ -93,6 +93,27 @@ public :
 	string word;
 };
 
+// ====== 클래스 생성자 초기화 리스트 ======
+class TmpClass
+{
+public :
+	TmpClass() : number1(10), number2(20), name("나운 : 문자명왕 이름")
+	{
+
+	}
+
+	void Print()
+	{
+		cout << number1 << ", " << number2 << ", " << number3 << ", " << name << endl;
+	}
+
+private :
+	int number1 = 1;
+	const int number2 = 2;
+	static const int number3 = 3;
+	string name = "조다 : 장수왕 아들";
+};
+
 // =======================================================================================================
 
 int main()
@@ -126,6 +147,11 @@ int main()
 
 	Class2* class2 = new Class2();
 	cout << "Class2 : " << class2->number << ", " << class2->prime << ", " << class2->word << endl;
+
+	// ====== 클래스 생성자 초기화 리스트 ======
+	cout << endl << "======클래스 생성자 초기화 리스트======" << endl;
+	TmpClass tc;
+	tc.Print();
 
 	return 0;
 }
